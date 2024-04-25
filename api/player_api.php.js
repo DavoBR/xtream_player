f5c232c7d38ff1302b6ff5016859f698d9a98031
David Berroa <davbr87@gmail.com>
2024-04-24 23:03:31 -0400

    refactor: hide credentials info from player api response

--- api/player_api.php.js
+++ api/player_api.php.js
@@ -4,10 +4,21 @@ export const config = {
 
 export default async function handler(request) {
     const params = new URL(request.url).searchParams
-    const actionUrl = `${params.get('serverUrl')}/player_api.php?${params}`
 
     try {
-        return await fetch(actionUrl)
+        const resp = await fetch(`${params.get('serverUrl')}/player_api.php?${params}`)
+
+        if (!params.get('action')) {
+            const payload = await resp.json()
+            delete payload['user_info']['username']
+            delete payload['user_info']['password']
+            return new Response(JSON.stringify(payload), {
+                status: resp.status,
+                headers: { 'content-type': 'application/json' }
+            })
+        }
+
+        return resp;
     } catch (e) {
         return new Response(JSON.stringify({
             error: e.toString()
